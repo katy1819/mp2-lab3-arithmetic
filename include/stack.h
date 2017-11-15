7f74eeedce715dcf72dc2d2e667a32acbd8fafd2
katy1819 <katerinka.fedotova1819@gmail.com>
2017-11-15 16:03:03 +0300

    Стек и тесты к нему

--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,202 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+
+#ifndef __TSTACK_H__
+#define __TSTACK_H__
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX_STACK_SIZE = 1000;
+
+// Шаблон стека
+template <class T>
+class TStack
+{
+protected:
+	T *pStack;
+	int Size;
+	int top;
+public:
+	TStack(int s = 10);
+	TStack(const TStack &st);
+	~TStack();
+	//T & operator[](int pos);
+	bool operator==(const TStack &st) const;
+	TStack & operator=(const TStack &st);
+	void Push(T Val); //вставка 
+	T Pop(); // извлечение
+	T Top(); //просмотр верхнего эл-та
+	bool IsEmpty(); // проверка на пустоту
+	bool IsFull(); //проверка на полноту
+	int Quantity(); // получение кол-ва эл-в
+	void Clean();
+};
+
+/*-------------------------------------------------------------------------*/
+
+template <class T>
+TStack<T>::TStack(int s) //тесты сделаны
+{
+	Size = s;
+	if(Size<0 || Size>MAX_STACK_SIZE)
+		throw "error";
+	pStack = new T[s];
+	top = 0;
+}
+
+/*-------------------------------------------------------------------------*/
+
+template <class T>
+TStack<T>::TStack(const TStack<T> &st) //тесты сделаны
+{
+	Size = st.Size;
+	top = st.top;
+	pStack = new T[Size];
+	for(int i = 0; i<top; i++)
+		pStack[i] = st.pStack[i];
+}
+
+/*-------------------------------------------------------------------------*/
+
+template <class T>
+TStack<T>::~TStack()
+{
+	delete [] pStack;
+}
+
+/*-------------------------------------------------------------------------*/
+
+template <class T>
+TStack<T> & TStack<T>::operator=(const TStack &st) //тесты сделаны
+{
+	if(this!=&st)
+	{
+		if(Size != st.Size)
+		{
+			delete [] pStack;
+			pStack = new T [st.Size];
+		}
+		Size = st.Size;
+		top = st.top;
+		for(int i=0; i<top; i++)
+			pStack[i] = st.pStack[i];
+	}
+	return *this;
+}
+
+/*-------------------------------------------------------------------------*/
+
+template <class T>    
+bool TStack<T>::operator==(const TStack &st) const  //тесты сделаны
+{
+	if(this!=&st)
+	{
+		if((Size == st.Size) && (top == st.top))
+		{
+			for(int i=0; i<top; i++)
+				if(pStack[i] != st.pStack[i])
+					return false;
+			return true;
+		}
+		else
+			return false;
+	}
+	return true;
+}
+
+/*-------------------------------------------------------------------------*/
+
+template <class T>
+bool TStack<T>::IsFull() //тесты сделаны
+{
+	if(top == Size)
+		return true;
+	else
+		return false;
+}
+
+/*-------------------------------------------------------------------------*/
+
+template <class T>
+bool TStack<T>::IsEmpty() //тесты сделаны
+{
+	if(top==0)
+		return true;
+	else
+		return false;
+}
+
+/*-------------------------------------------------------------------------*/
+
+template<class T>
+void TStack<T>::Push(T Val) //тесты сделаны
+{
+	if((Size - top)==0)
+	{
+		T* st1;
+		st1 = new T [2*Size];
+		for( int i=0; i<top; i++)
+			st1[i] = pStack[i];
+		delete [] pStack;
+		pStack = st1;
+
+		pStack[top] = Val;
+		top++;
+
+		Size = 2*Size;
+	}
+	else
+	{ 
+		pStack[top] = Val; 
+		top+=1;
+	}
+}
+
+/*-------------------------------------------------------------------------*/
+
+template<class T>
+T TStack<T>::Pop() //тесты сделаны
+{
+	if (!IsEmpty())
+	{
+		top = top-1;
+		return pStack[top];
+	}
+	else 
+		throw "empty";
+}
+
+/*-------------------------------------------------------------------------*/
+
+template<class T>
+T TStack<T>::Top() //тесты сделаны
+{
+	if (!IsEmpty())
+	{
+		return pStack[top - 1];
+	}
+	else throw "empty";
+}
+
+/*-------------------------------------------------------------------------*/
+
+template<class T>
+int TStack<T>::Quantity() //тесты сделаны
+{
+	return top;
+}
+
+/*-------------------------------------------------------------------------*/
+
+template<class T>
+void TStack<T>::Clean() //тесты сделаны
+{
+	top = 0;
+}
+
+/*-------------------------------------------------------------------------*/
+
+#endif
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -2,3 +2,182 @@
 
 #include "stack.h"
 #include <gtest.h>
+
+TEST(TStack, can_create_stack_with_positive_lenght)
+{
+	ASSERT_NO_THROW(TStack<int> st(5));
+}
+
+TEST(TStack, can_create_too_large_stack)
+{
+	ASSERT_ANY_THROW(TStack<int> st(MAX_STACK_SIZE+1));
+}
+
+TEST(TStack, throws_when_create_stack_with_negative_length)
+{
+	ASSERT_ANY_THROW(TStack<int> st(-5));
+}
+
+TEST(TStack, can_create_copied_stack)
+{
+	TStack<int> st(10);
+
+	ASSERT_NO_THROW(TStack<int> st1(st));
+}
+
+TEST(TStack, can_not_delete_if_stack_is_empty)
+{
+	TStack<int> st(1);
+	st.Push(1);
+	st.Pop();
+	st.IsEmpty();
+
+	ASSERT_ANY_THROW(st.Pop());
+}
+
+TEST(TStack, can_not_see_the_top_element_if_stack_is_empty)
+{
+	TStack<int> st(1);
+	st.Push(1);
+	st.Pop();
+
+	ASSERT_ANY_THROW(st.Top());
+}
+
+TEST(TStack, copied_stack_is_equal_to_source_one) 
+{
+	TStack<int> st1(4);
+	for(int i=0; i<4; i++)
+		st1.Push(i);
+	TStack<int> st2(st1);
+	EXPECT_EQ(st1, st2);
+} 
+
+TEST(TStack, can_get_quantity)
+{
+	TStack<int> st(4);
+	for(int i = 0; i<4; i++)
+		st.Push(i);
+
+	EXPECT_EQ(4, st.Quantity());
+}
+
+TEST(TStack, can_push_elem1)
+{
+	TStack<int> st(3);
+	st.Push(1);
+	st.Push(2);
+	st.Push(3);
+	st.Push(4);
+
+	EXPECT_EQ(4, st.Top());
+}
+
+TEST(TStack, can_push_elem2)
+{
+	TStack<int> st(4);
+	st.Push(1);
+	st.Push(2);
+	st.Push(3);
+	st.Push(4);
+
+	EXPECT_EQ(4, st.Top());
+}
+
+TEST(TStack, can_pop_elem)
+{
+	TStack<int> st(2);
+	st.Push(1);
+	st.Push(2);
+	st.Pop();
+
+	EXPECT_EQ(1, st.Top());
+}
+
+TEST(TStack, can_check_for_emptiness)
+{
+	TStack<int> st(2);
+	st.Push(1);
+	st.Push(2);
+	 
+	EXPECT_NE(true, st.IsEmpty());
+}
+
+TEST(TStack, can_check_for_completeness)
+{
+	TStack<int> st(2);
+	st.Push(1);
+	st.Push(2);
+
+	EXPECT_EQ(true, st.IsFull());
+}
+
+TEST(TStack, can_not_compare_stacks_of_different_sizes)
+{
+	TStack<int> st1(1), st2(2);
+	st1.Push(1);
+	st2.Push(1);
+	st2.Push(2);
+
+	EXPECT_NE(true, st1 == st2);
+}
+
+TEST(TStack, can_compare_stacks_of_different_sizes1)
+{
+	TStack<int> st1(2), st2(2);
+	st1.Push(1);
+	st1.Push(2);
+	st2.Push(1);
+	st2.Push(2);
+
+	EXPECT_EQ(true, st1 == st2);
+}
+
+TEST(TStack, can_compare_stacks_of_different_sizes_but_with_different_numbers)
+{
+	TStack<int> st1(2), st2(2);
+	st1.Push(1);
+	st1.Push(3);
+	st2.Push(1);
+	st2.Push(2);
+
+	EXPECT_NE(true, st1 == st2);
+}
+
+TEST(TStack, can_return_the_number_of_elements)
+{
+	TStack<int> st(4);
+	st.Push(1);
+	st.Push(3);
+	st.Push(1);
+	st.Push(2);
+	st.Clean();
+
+	EXPECT_EQ(true, st.IsEmpty());
+}
+
+TEST(TStack, can_equate_stacks_of_different_sizes)
+{
+	TStack<int> st(3), st1(2);
+	st1.Push(1);
+	st1.Push(2);
+	st.Push(1);
+	st.Push(2);
+	st.Push(3);
+
+	st = st1;
+
+	EXPECT_EQ(st1, st);
+}
+
+TEST(TStack, can_equate_equal_stacks)
+{
+	TStack<int> st1(2);
+	st1.Push(1);
+	st1.Push(2);
+
+	st1 = st1;
+	EXPECT_EQ(st1, st1);
+}
+
+
