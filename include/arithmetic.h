d726a19ea020178c91b10309e24c7eb4aff84e4d
katy1819 <katerinka.fedotova1819@gmail.com>
2017-11-16 17:47:48 +0300

    Начало лексем

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,23 @@
 // объ€вление функций и классов дл€ вычислени€ арифметических выражений
+
+#include <iostream>
+
+using namespace std;
+
+enum LexType {OP_BR, CL_BR, VAR, VAL, OPER}; 
+
+struct Lexem
+{
+	char str[10];
+	LexType type;
+	Lexem(char *s, LexType t);
+	Lexem(const Lexem &p);
+	Lexem & operator=(const Lexem &p);
+	bool operator==(const Lexem &p) const;
+};
+
+
+class Ariphmetic
+{
+
+};
